248c03bbaad29c8248acc76ba691ece7519ab39b
ahmedmuti <ahmedmutti@gmail.com>
2024-07-29 15:44:04 +0300

    new upadte

--- index.js
+++ index.js
@@ -7,8 +7,25 @@ import cors from "cors"
 // import dotenv from "dotenv"
 // dotenv.config()
 import 'dotenv/config'
+import { catchError } from './src/middleware/catchError.js'
 const app = express()
 const port = process.env.PORT || 3000
+
+
+
+app.post('/api/webhook', express.raw({ type: 'application/json' }), catchError((req, res) => {
+    const sig = req.headers['stripe-signature'].toString()
+
+    let event = stripe.webhooks.constructEvent(req.body, sig, "whsec_nIgLX84pCoAME8LMZN71xy7cyoQSj8rc");
+    let checkout
+    if (event.type == "checkout.session.completed") {
+        checkout = event.data.object;
+    }
+    res.json({ message: "success", checkout });
+}));
+
+
+
 app.use(cors())
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))

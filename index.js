be3c7ba46f93d2886404293b3bbed351d3a24d16
ahmedmuti <ahmedmutti@gmail.com>
2024-07-29 16:04:49 +0300

    new session

--- index.js
+++ index.js
@@ -9,15 +9,48 @@ const stripe = new Stripe('sk_test_51PgRqfKjuca3jxtgL39PODU7ct8qKOFouNAYRaBC3gqt
 
 import 'dotenv/config'
 import { catchError } from './src/middleware/catchError.js'
+import { Order } from './database/models/order.model.js'
+import { User } from './database/models/user.model.js'
+import { Cart } from './database/models/cart.model.js'
+import { Product } from './database/models/product.model.js'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.post('/api/webhook', express.raw({ type: 'application/json' }), catchError((req, res) => {
+app.post('/api/webhook', express.raw({ type: 'application/json' }), catchError(async (req, res) => {
     const sig = req.headers['stripe-signature'].toString()
     let event = stripe.webhooks.constructEvent(req.body, sig, "whsec_ZkhRwZKMxgwTMCZTDN197K2wmbeBJbRh");
     let checkout
     if (event.type == "checkout.session.completed") {
         checkout = event.data.object;
+
+        let user = await User.findOne({ email: checkout.customer_email })
+        let cart = await Cart.findById(checkout.client_reference_id)
+        if (!cart) return next(new AppError('cart not found', 404))
+        let order = new Order({
+            user: user._id,
+            orderItems: cart.cartItems,
+            shippingAddress: checkout.metadata,
+            totalOrderPrice: checkout.amount_total / 100,
+            paymentType: 'card',
+            isPaid: true
+        })
+        await order.save()
+        //4-increment sold & decrement stock
+        let options = cart.cartItems.map((prod) => {
+            return (
+                {
+                    updateOne: {
+                        "filter": { _id: prod.product },
+                        "update": { $inc: { sold: prod.quantity, stock: -prod.quantity } }
+                    }
+                }
+            )
+        })
+        await Product.bulkWrite(options)
+        //5-clear user cart
+        await Cart.findByIdAndDelete(cart._id)
+
+
     }
     res.json({ message: "success", checkout });
 }));
